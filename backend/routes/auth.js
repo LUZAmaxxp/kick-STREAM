551300e494b28e9c1832b2ce2d95aeae9b0598cc
LUZAmaxxp <allouchayman21@gmail.com>
2026-05-30 12:26:55 +0100

    feat

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,8 @@
 function setTokenCookie(res, token) {
   res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 60 * 60 * 1000, // 1 hour
   });
 }
